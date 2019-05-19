b8a053dc5ce9188f3e30d26695420321bca0b26c
lisa <lisa.schneider@stud.fh-campuswien.ac.at>
2019-05-19 21:27:38 +0200

    Upload assignment 3

--- assignment3.py
+++ assignment3.py
@@ -1,13 +1,8 @@
 #! /usr/bin/env python3
 
-import importlib
 import vcf
 import httplib2
 import json
-import numpy as np
-import re
-import os
-import functools
 
 __author__ = 'Lisa Schneider'
 
@@ -25,32 +20,27 @@ __author__ = 'Lisa Schneider'
 ##
 
 class Assignment3:
-    
+
     def __init__(self, in_vcf):
-        self.vcf_path = in_vcf
-        self.annotation_data = self.annotate_vcf_file(self.vcf_path)
-
-        # with open(f"{self.vcf_path}_annotation.txt", "w") as annotation_writer:
-        #     for item in self.annotation_data:
-        #         annotation_writer.write("%s\n" % item)
-
         ## Check if pyvcf is installed
         print("PyVCF version: %s" % vcf.VERSION)
 
-    def annotate_vcf_file(self, vcf_path):
+        ## Call annotate_vcf_file here
+        self.vcf_path = in_vcf  # TODO
+        self.annotation_data = self.annotate_vcf_file()
+
+    def annotate_vcf_file(self):
         '''
         - Annotate the VCF file using the following example code (for 1 variant)
         - Iterate of the variants (use first 900)
         - Store the result in a data structure
-        :return:
+        :return: annotation data
         '''
-
         ## Build the connection
         h = httplib2.Http()
         headers = {'content-type': 'application/x-www-form-urlencoded'}
-
         params_pos = []  # List of variant positions
-        with open(vcf_path) as my_vcf_fh:
+        with open(self.vcf_path) as my_vcf_fh:
             vcf_reader = vcf.Reader(my_vcf_fh)
             for counter, record in enumerate(vcf_reader):
                 params_pos.append(record.CHROM + ":g." + str(record.POS) + record.REF + ">" + str(record.ALT[0]))
@@ -69,11 +59,10 @@ class Assignment3:
 
         return annotation_dataset
 
-    
     def get_list_of_genes(self, annotation_data):
         '''
         Print the name of genes in the annotation data set
-        :return:
+        :return: Nothing
         '''
         gene_list = []
         for line in annotation_data:
@@ -84,12 +73,10 @@ class Assignment3:
         print(f"List of genes: \n"
               f"\t{gene_string}")
 
-
-    
     def get_num_variants_modifier(self, annotation_data):
         '''
         Print the number of variants with putative_impact "MODIFIER"
-        :return:
+        :return: Nothing
         '''
         counter = 0
         for line in annotation_data:
@@ -98,12 +85,11 @@ class Assignment3:
                     if line['snpeff']['ann']['putative_impact'] == 'MODIFIER':
                         counter += 1
         print("Number of variants with putative impact 'MODIFIER': ", counter)
-        
-    
+
     def get_num_variants_with_mutationtaster_annotation(self, annotation_data):
         '''
         Print the number of variants with a 'mutationtaster' annotation_data
-        :return:
+        :return: Nothing
         '''
         counter = 0
         for line in annotation_data:
@@ -112,11 +98,10 @@ class Assignment3:
                     counter += 1
         print(f"Number of variants with mutationtaster annotation_data: {counter}")
 
-    
     def get_num_variants_non_synonymous(self, annotation_data):
         '''
         Print the number of variants with 'consequence' 'NON_SYNONYMOUS'
-        :return:
+        :return: Nothing
         '''
         counter = 0
         for line in annotation_data:
@@ -125,38 +110,33 @@ class Assignment3:
                     if line['cadd']['consequence'] == "NON_SYNONYMOUS":
                         counter += 1
         print(f"Number of non synonymous variants: {counter}")
-        
-    
-    def view_vcf_in_browser(self):
-        '''
-        - Open a browser and go to https://vcf.iobio.io/
-        - Upload the VCF file and investigate the details
-        :return:
-        '''
-        print("The vcf file was compressed and indexed and the results were visualized with the vcf.iobio website. \n"
-              "\thttps://vcf.iobio.io/?species=Human&build=GRCh38.")
-            
-    
+
+    def view_vcf_in_browser(self):  # todo does not display anything
+        ## Document the final URL here
+        print("The vcf file was compressed and indexed and the results were visualized with the vcf.iobio website.\n" 
+              "\t https://vcf.iobio.io/?species=Human&build=GRCh38")
+
+
     def print_summary(self):
+        # dataset = self.annotate_vcf_file()
         print("Print all results here")
         self.get_list_of_genes(self.annotation_data)
         self.get_num_variants_modifier(self.annotation_data)
         self.get_num_variants_with_mutationtaster_annotation(self.annotation_data)
         self.get_num_variants_non_synonymous(self.annotation_data)
         self.view_vcf_in_browser()
-    
-    
+
+
 def main():
     print("Assignment 3")
     assignment3 = Assignment3("chr16.vcf")
     assignment3.print_summary()
     print("Done with assignment 3")
-        
-        
+
+
 if __name__ == '__main__':
     main()
-   
-    
+
 
 
 
